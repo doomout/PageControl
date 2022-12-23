bb41ae95c80a83d02a7a2debbbdf1e7514f0b7e6
doomout <winquake3@hanmail.net>
2022-12-23 19:12:23 +0900

    이미지 추가, readme update, 코드 완성

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -7,18 +7,28 @@
 
 import UIKit
 
+var images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png" ]
+
 class ViewController: UIViewController {
 
-    @IBOutlet var imgView: UIImageView!
-    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var imgView: UIImageView! //이미지 출력용 아웃렛 변수
+    @IBOutlet var pageControl: UIPageControl! //페이지 컨트롤용 아웃렛 변수
     
+    //뷰가 로드되었을 때 호출됨
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count //페이지 컨트롤의 전체 페이지 수
+        pageControl.currentPage = 0 //현재 페이지는 0
+        pageControl.pageIndicatorTintColor = UIColor.green //페이지 컨트롤의 페이지 표시 부분의 색상
+        pageControl.currentPageIndicatorTintColor = UIColor.red //현재 페이지 컨트롤 페이지의 색상
+        imgView.image = UIImage(named: images[0])
     }
 
 
     @IBAction func pageChange(_ sender: UIPageControl) {
+        //페이지 컨트롤의 현재 페이지를 가져와서 images배열에 넣고, UIImage 타입의 이미지를 만들어 이미지 뷰에 할당한다.
+        imgView.image = UIImage(named: images[pageControl.currentPage])
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 #Page Control  
 페이지 컨트롤이란? 여러개의 내용을 페이지별로 보여주기 위해 사용하는 객체. 하단에 점으로 표시 되어 있는 객체이다.  
+-스위프트 문법-  
+1. 색상을 표현하는 방법
+(1) 스위프트에서 정의된 메서드를 사용하는 방법     
+-색상의 단어를 이용하여 정의하는 법    
+(예)     
+let red = UIColor.red   
+let black = UIColor.black  
+ 
+(2) 삼원색인 RGB와 투명도 Alpha 값을 사용하는 방법  
+(형식) 
+//RGB 부분은 0~1 사이의 실수 값을 가진다. 색상표의 값을 255로 나누어 표시한다.  
+//투명도는 0~1 사이의 실수 값을 가진다. 0은 투명, 1은 불투명이다.  
+init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)   
+
+(예) 
+let red = UIColor(red: 1, green: 0, blue: 0, alpha: 1) //빨강, 불투명    
+let myColor = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)    
+
